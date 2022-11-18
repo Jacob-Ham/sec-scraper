5cc78f85f9983b9e0cdf34140250e68467e0c7c3
Jacobh <jacoblh@duck.com>
2022-11-18 14:04:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# sec scraper
+
+Created to practice interacting with json API's with python.
+currently returns only the first instance of a given company. 
+
+Input a compan's name and receive its address! 
+
+*to comply with the SEC rules regarding scraping, you must provide
+your company name and contact email in the headers of your request. 
+ 
---
+++ scraper.py
@@ -0,0 +1,36 @@
+#!/bin/sh
+import requests, json, re
+
+
+def main():
+    name = input("Company name: ")
+    address = get_data(cik_lookup(name))
+    for i in address.values():
+        print(i)
+    
+def get_data(cik):
+    cSlice = cik[0]
+    cleanZero = 0
+    if(len(cik) < 10):
+        cleanZero = 10 - len(cSlice)
+        for i in range(cleanZero):
+            cSlice = '0' + cSlice
+
+    url = (f'https://data.sec.gov/submissions/CIK{cSlice}.json')
+    headers = {'User-Agent': '<YOUR NAME> <YOUR EMAIL>', 'Accept-Encoding': 'gzip, deflate'}
+    r = requests.get(url, headers=headers).text
+    data = json.loads(r)
+    address = {'street' : data['addresses']['business']['street1'],
+    'city' : data['addresses']['business']['city'],
+    'state' : data['addresses']['business']['stateOrCountry'],
+    'zip' : data['addresses']['business']['zipCode'] }
+    return address
+
+def cik_lookup(name):
+    url = f'https://www.sec.gov/cgi-bin/browse-edgar?company={name}&match=starts-with&filenum=&State=&Country=&SIC=&myowner=exclude&action=getcompany'
+    headers = {'User-Agent': '<Company name> <your@email.com>', 'Accept-Encoding': 'gzip, deflate'}
+    r = requests.get(url, headers=headers).text
+    cik = re.findall('(?<=CIK=)[^&]*', r)
+    return cik
+
+main()
